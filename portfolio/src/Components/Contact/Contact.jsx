7d3c2c4eef3660d9c2a99d5b5b1112532118e32d
MohitGupta10 <mg08312@gmail.com>
2022-02-12 13:26:14 +0530

    Update2

--- portfolio/src/Components/Contact/Contact.jsx
+++ portfolio/src/Components/Contact/Contact.jsx
@@ -41,7 +41,7 @@ const footerData = [
     id: 2,
     title: "Github",
     display: "Github",
-    link: "https://github.com/12Mohit-Gupta",
+    link: "https://github.com/MohitGupta10",
     icon: <FaGithub className="commonIconsFooter" />,
   },
   {
