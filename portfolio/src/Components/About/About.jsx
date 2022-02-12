8409733cf50e6aecc731a0f6f3c7a9d6e805a30d
MohitGupta10 <mg08312@gmail.com>
2022-02-12 13:04:55 +0530

    Portfolio

---
+++ portfolio/src/Components/About/About.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { FaTwitter, FaGithub, FaLinkedin } from "react-icons/fa";
+import { SiLeetcode } from "react-icons/si";
+import {
+  AboutDiv,
+  Column,
+  Img,
+  AboutInfo,
+  AboutInfoP,
+  HeaderUl,
+  HeaderLi,
+  JelloHori,
+} from "./Styles";
+
+import { Container, H1 } from "../../SharedStyles/SharedStyles";
+const socialMediaLinks = [
+  {
+    id: 0,
+    link: "https://www.linkedin.com/in/mohit-gupta-5b22a0118/",
+    icon: <FaLinkedin />,
+  },
+  {
+    id: 1,
+    link: "https://github.com/12Mohit-Gupta",
+    icon: <FaGithub />,
+  },
+ 
+];
+const About = () => {
+  return (
+    <Container className="about">
+      <br />
+      <AboutDiv>
+        <Column>
+          <Img src="/images/Mohit.webp" alt="my image" />
+        </Column>
+        <Column>
+          <H1>About Me</H1>
+          <AboutInfo>
+            <AboutInfoP>
+              A dedicated Full Stack Web developer with a specialized skill,After completing my graduation, I decided to learn full-stack web development thus I joined Masai School. I am proficient in JavaScript, ExpressJs, Node.js, HTML, and CSS. I also have the proven ability 
+              to work efficiently in teams and solve problems analytically.
+            </AboutInfoP>
+            <AboutInfoP>
+              I am problem-solving and analytics, I am passionate about solving
+              Algorithmic problems with a good hold on Data Structures.
+            </AboutInfoP>
+          </AboutInfo>
+          <HeaderUl>
+            {socialMediaLinks.map((item) => (
+              <JelloHori key={item.id}>
+                <a href={item.link} target="_blank" rel="noopener noreferrer">
+                  <HeaderLi>{item.icon}</HeaderLi>
+                </a>
+              </JelloHori>
+            ))}
+          </HeaderUl>
+        </Column>
+      </AboutDiv>
+      <br />
+    </Container>
+  );
+};
+
+export default About;
---
+++ portfolio/src/Components/Projects/Projects.jsx
@@ -0,0 +1,104 @@
+import React from "react";
+import { FaGithub, FaLink, FaNodeJs } from "react-icons/fa";
+import {
+  ProjectDiv,
+  Row,
+  ServiceBox,
+  ServiceBoxHeader,
+  ServiceBoxP,
+  A,
+  BoxDiv,
+} from "./Styles";
+import { Container, Common, H1 } from "../../SharedStyles/SharedStyles";
+import { FaHtml5, FaReact, FaJs, FaCss3 } from "react-icons/fa";
+import { SiMongodb, SiRedux } from "react-icons/si";
+const Projects = () => {
+  const projects = [
+    {
+      id: 1,
+      project_name: "Lakme India Clone",
+      image: "/project_images/LakmeIndia.png",
+      project_desc:
+        "A web application to purchase high-end cosmetic products.Built using js,css,html",
+      deploy_link: "https://lakme-india-clone-jp.netlify.app/",
+      repo_link: "https://github.com/12Mohit-Gupta/Consruct-Week-Lakme-Project",
+      tech_stack: [
+        <FaJs />,
+        <FaHtml5 />,
+        <FaCss3 />,
+      ],
+    },
+    
+    {
+      id: 2,
+      project_name: "GROWW Clone",
+      image: "/project_images/GROWW.png",
+      project_desc:
+        "An online stocks trading web app. Built using JS, Express js, Node js and MongoDB.",
+      deploy_link: "https://groww-clone-u3.netlify.app/",
+      repo_link: "https://github.com/12Mohit-Gupta/Clone-of-Groww",
+      tech_stack: [
+        <FaHtml5 />,
+        <FaCss3 />,
+        <FaJs />,
+        <FaNodeJs />,
+        <SiMongodb />,
+        <FaNodeJs />,
+      ],
+    },
+  
+   ];
+  
+  return (
+    <Container className="projects">
+      <ProjectDiv>
+        <Common>
+          <H1>Projects</H1>
+        </Common>
+        <Row>
+          {projects.map((item) => (
+            <BoxDiv key={item.id}>
+              <ServiceBox>
+                <img src={item.image} alt="icon" style={{ width: "100%" }} />
+                <div style={{ margin: "15px" }}>
+                  <ServiceBoxHeader style={{ textAlign: "center" }}>
+                    {item.project_name}
+                  </ServiceBoxHeader>
+                  <ServiceBoxP>{item.project_desc}</ServiceBoxP>
+                  {/* <a href="">Read more</a> */}
+                  <div
+                    style={{
+                      margin: "15px 20px",
+                      display: "flex",
+                      justifyContent: "center",
+                    }}
+                  >
+                    {item.tech_stack.map((item) => (
+                      <div className="iconsTechStack">{item}</div>
+                    ))}
+                  </div>
+                  <A
+                    href={item.repo_link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <FaGithub /> Code
+                  </A>
+                  <A
+                    href={item.deploy_link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <FaLink /> Demo
+                  </A>
+                </div>
+              </ServiceBox>
+            </BoxDiv>
+          ))}
+        </Row>
+      </ProjectDiv>
+    </Container>
+  );
+};
+
+export default Projects;
